f9ed582b5e8004c3a6235ea494a316580cf0794d
kamarbelhadj <belhadj.kamar02@gmail.com>
2024-09-21 01:00:43 +0200

    mm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/navBar";
+import NavBar from "./components/NavBar";
+import Home from "./pages/home";
+import Collection from "./pages/collection";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Product from "./pages/product";
+import Cart from "./pages/cart";
+import Login from "./pages/login";
+
+
 
 
 
@@ -10,14 +19,15 @@ const App = () => {
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/Collection" element={<Collection />}></Route>
-        <Route path="/About" element={<About />}></Route>
-        <Route path="/Contact" element={<Contact />}></Route>
-        <Route path="/Product/:productId" element={<Product />}></Route>
-        <Route path="/Cart" element={<Cart />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-        <Route path="/Place-order" element={<PlaceOrder />}></Route>
-        <Route path="/Orders" element={<Orders />}></Route>
+        <Route path="/collection" element={<Collection />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/aroduct/:productId" element={<Product />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/place-order" element={<PlaceOrder />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+
       </Routes>
     </div>
   );
